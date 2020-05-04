c523c5ef8f61e218124679dbe498b8e909b46281
j4dream <j4dream@126.com>
2020-05-04 23:55:15 +0800

    Updates

---
+++ precache-manifest.c65c77f4eb07350fcc5a72646b30dcfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2f1dd27928b79d490ba6c8f1eab02f",
+    "url": "/cs-table/index.html"
+  },
+  {
+    "revision": "507932b903486e117fcd",
+    "url": "/cs-table/static/css/main.81bf58ce.chunk.css"
+  },
+  {
+    "revision": "80fdd73b0cb026769d2e",
+    "url": "/cs-table/static/js/2.11b47a57.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/cs-table/static/js/2.11b47a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507932b903486e117fcd",
+    "url": "/cs-table/static/js/main.4b0cc16f.chunk.js"
+  },
+  {
+    "revision": "3bbee098f7c327e2b32d",
+    "url": "/cs-table/static/js/runtime-main.9eec4312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs-table/static/media/logo.5d5d9eef.svg"
+  }
+]);

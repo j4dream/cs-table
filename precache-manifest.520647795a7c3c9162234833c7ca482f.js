999dfc4403cd1fb9d89ac5cf048dd198e3109322
j4dream <j4dream@126.com>
2020-05-05 21:32:20 +0800

    Updates

--- precache-manifest.520647795a7c3c9162234833c7ca482f.js
+++ precache-manifest.520647795a7c3c9162234833c7ca482f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ae2f1dd27928b79d490ba6c8f1eab02f",
+    "revision": "af445b34bc81dbc35e17637d420c3123",
     "url": "/cs-table/index.html"
   },
   {
-    "revision": "507932b903486e117fcd",
+    "revision": "dfc0d81820eac393fb12",
     "url": "/cs-table/static/css/main.81bf58ce.chunk.css"
   },
   {
-    "revision": "80fdd73b0cb026769d2e",
-    "url": "/cs-table/static/js/2.11b47a57.chunk.js"
+    "revision": "8dd3a6b69cd261a207d5",
+    "url": "/cs-table/static/js/2.1bd2076e.chunk.js"
   },
   {
     "revision": "f86a4e17d3cb58f717d9f416937db9aa",
-    "url": "/cs-table/static/js/2.11b47a57.chunk.js.LICENSE.txt"
+    "url": "/cs-table/static/js/2.1bd2076e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "507932b903486e117fcd",
-    "url": "/cs-table/static/js/main.4b0cc16f.chunk.js"
+    "revision": "dfc0d81820eac393fb12",
+    "url": "/cs-table/static/js/main.403eb85c.chunk.js"
   },
   {
     "revision": "3bbee098f7c327e2b32d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs-table/precache-manifest.c65c77f4eb07350fcc5a72646b30dcfb.js"
+  "/cs-table/precache-manifest.520647795a7c3c9162234833c7ca482f.js"
 );
 
 self.addEventListener('message', (event) => {

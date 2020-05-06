10a3ed3672999512a0a79109f8236879cdf50abc
j4dream <j4dream@126.com>
2020-05-06 19:10:33 +0800

    fea: init sheet

---
+++ src/sheet/useTree.js
@@ -0,0 +1,9 @@
+import { useRef } from "react";
+
+export default function useTree() {
+  const children = useRef([]);
+  const deepestNodePath = useRef([]);
+  const leafNodes = useRef([]);
+
+  
+}

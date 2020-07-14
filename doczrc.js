55a99bbd82678a434ada8af20be9b908688fce19
j4dream <j4dream@126.com>
2020-07-14 15:08:51 +0800

    docs(stable): change sheet to stable

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,6 @@
 export default {
   base: '/cs-table',
-  title: 'Virtulized SC Table@0.4.0',
+  title: 'Virtulized SC Table@0.5.0',
   files: ['**/*.mdx'],  // should not find README.md
   ignore: ['README.md'],  // should also ignore README.md
 }

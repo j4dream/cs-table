6ef13929c2a6a1cc3b32f8ad7c2fa1423c00c826
j4dream <j4dream@126.com>
2021-08-03 19:02:46 +0800

    docs: remove docz

--- doczrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-  base: '/cs-table',
-  title: 'Virtulized SC Table@0.5.0',
-  files: ['**/*.mdx'],  // should not find README.md
-  ignore: ['README.md'],  // should also ignore README.md
-}
